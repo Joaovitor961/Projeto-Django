631a3093a469fe0e2ad4ad422e9e9b4bfdeb8392
Guilherme Cunha <alexandregui013@gmail.com>
2025-10-21 05:07:29 -0300

    Testando o projeto e começando a criar os modelos da tabela

--- code/proj_web/myapp/models.py
+++ code/proj_web/myapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Turma(models.Model):
+    nome = models.CharField(max_length=50, unique=True) 
+
+class Disciplina(models.Model):
+    nome = models.CharField(max_length=100, unique=True)
+
+class Professor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='professor')
+    departamento = models.CharField(max_length=255)
+
+class Aluno(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='aluno')
+    #user = models.CharField(max_length=255)
+    matricula = models.CharField(max_length=20, unique=True)
+    data_nascimento = models.DateField()
+    #turma = models.CharField(max_length=3)
+    turma = models.ForeignKey(Turma, on_delete=models.PROTECT, related_name='alunos')
--- code/proj_web/myapp/views.py
+++ code/proj_web/myapp/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+def teste(request):
+    template = loader.get_template('paginateste.html')
+    return HttpResponse(template.render())
+
+def testeparametros(request):
+    context = {
+        "nome": "José Silva",
+        "idade": 30,
+        "email": "jose.silva@email.com",
+        "telefone": "3333-1234",
+        "usuarioativo": True,
+        "condicional": 3,
+        'numeros': [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+    }
+    template = loader.get_template('testeparametros.html')
+    return HttpResponse(template.render(context, request))
