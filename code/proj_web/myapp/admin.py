468c80996f2840bb3ab026b1e4d9e2fd45dadf95
Joaovitor961 <joaovitorgarciamoreira@gmail.com>
2025-10-21 14:28:37 -0300

    Adcionando gitignore para privacidade de dados pessoais de hosteamento

--- code/proj_web/myapp/admin.py
+++ code/proj_web/myapp/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Turma, Disciplina, Professor, Aluno
 
-# Register your models here.
+@admin.register(Turma)
+class TurmaAdmin(admin.ModelAdmin):
+    list_display = ('nome',)
+
+@admin.register(Disciplina)
+class DisciplinaAdmin(admin.ModelAdmin):
+    list_display = ('nome',)
+
+@admin.register(Professor)
+class ProfessorAdmin(admin.ModelAdmin):
+    list_display = ('user',)
+
+@admin.register(Aluno)
+class AlunoAdmin(admin.ModelAdmin):
+    list_display = ('user', 'matricula', 'turma')
+    search_fields = ('user__username', 'matricula')
