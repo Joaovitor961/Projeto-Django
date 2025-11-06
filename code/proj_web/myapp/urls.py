bda79b2b67919ea3fcd057751e58bf8f63b9cd0e
Guilherme Cunha <alexandregui013@gmail.com>
2025-11-06 11:47:18 -0300

    Adicionando telas
    Criando a tela de erro(404), a tela base e a tela home(não finalizadas)

--- code/proj_web/myapp/urls.py
+++ code/proj_web/myapp/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   
+    path('', views.home, name='home'),
+
+    #Painel do aluno
+    #path('aluno/dashboard/', views.dashboard_aluno, name='dashboard_aluno'),
+
+    #Painel do professor
+    #path('professor/dashboard/', views.dashboard_professor, name='dashboard_professor'),
+    #path('professor/lancar/<int:turma_id>/<int:disciplina_id>/',views.lancar_notas, name='lancar_notas'),
 ]
--- code/proj_web/proj_web/settings.py
+++ code/proj_web/proj_web/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!+b7x3096y%j(mnbq4dimmo&!e_x0woq_86vfw!t-8v^ucrg8o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -117,6 +117,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'productionfiles'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'staticfiles'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
